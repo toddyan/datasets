0354f2cb35c16a6ceb2ea1ad22f820bfb55e8528
U-TENCENT\toddyan <toddyan@toddyan-PC2.tencent.com>
2018-02-06 19:55:08 +0800

    add iris loader

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,54 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 import numpy as np
-def load_mnist():
+
+def load_mnist(mnist_dir):
     trainData = []
-    with open("./mnist/train-data-1") as infile:
+    with open(mnist_dir + "/train-data-1") as infile:
         for line in infile.readlines():
             line = line.strip()
             trainData.append([float(s) for s in line.split('\t')])
-    with open("./mnist/train-data-2") as infile:
+    with open(mnist_dir + "/train-data-2") as infile:
         for line in infile.readlines():
             line = line.strip()
             trainData.append([float(s) for s in line.split('\t')])
-    return np.array(trainData)
+
+    trainLabel = []
+    with open(mnist_dir + "/train-label") as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            trainLabel.append([float(s) for s in line.split('\t')])
+
+    testData = []
+    with open(mnist_dir + "/test-data") as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            testData.append([float(s) for s in line.split('\t')])
+
+    testLabel = []
+    with open(mnist_dir + "/test-label") as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            testLabel.append([float(s) for s in line.split('\t')])
+
+    return {"train-data": np.array(trainData),
+            "train-label": np.array(trainLabel),
+            "test-data": np.array(testData),
+            "test-label": np.array(testLabel)}
+
+def load_iris(iris_dir):
+    data = []
+    with open(iris_dir + "/data") as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            data.append([float(s) for s in line.split('\t')])
+    label = []
+    with open(iris_dir + "/target") as infile:
+        for line in infile.readlines():
+            line = line.strip()
+            label.append([float(s) for s in line.split('\t')])
+    return {"data":np.array(data),"label":np.array(label)}
+
+
+
 
